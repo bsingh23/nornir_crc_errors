dd3f1b5d3c631479079e1ffc63ba5478b8456778
Bharat Singh Panwar <55885130+bsingh23@users.noreply.github.com>
2023-04-27 13:11:58 +0200

    Update crc_error_v1.0.py
    Added RICH table for output generation on screen

--- crc_error_v1.0.py
+++ crc_error_v1.0.py
@@ -4,19 +4,30 @@ from nornir_scrapli.tasks import send_command
 from datetime import datetime
 import os
 import csv
+from rich.console import Console, ConsoleThreadLocals
+from rich.table import Table
+from rich import print as rprint
 
-nr = InitNornir(config_file="/net_ops/nornir/config_test.yaml")
+nr = InitNornir(config_file="/net_ops/nornir/config_prod.yaml")
 
-# Define the header row for the CSV file
+#Define the header row for the CSV file
 header = ["Hostname", "Interface", "CRC Error"]
 
-# Get the current date and time as a string to use in the file name
+#Define the parametsr for RICH Table creation
+table = Table(title="Device CRC ERROR Details \n")
+table.add_column("Hostname", justify="right", style="cyan", no_wrap=True)
+table.add_column("Interface", justify="right", style="yellow", no_wrap=True)
+table.add_column("CRC-Error", style="bold red")
+
+rprint(f"[gold3] Data is being collected. Please wait !!! \n [/gold3]")
+
+#Get the current date and time as a string to use in the file name
 time_now = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# Create the file name using the current date and time
+#Create the file name using the current date and time
 filename = (f"crcerror_{time_now}.csv")
 
-# Check if the file already exists; if not, create the file and write the header row
+#Check if the file already exists; if not, create the file and write the header row
 if not os.path.isfile(filename):
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
@@ -29,14 +40,24 @@ def crc_error(task):
     
     hostname = task.host.name
 
-    # Filter interfaces that have CRC errors (List comprehension)
+    #Filter interfaces that have CRC errors (List comprehension)
     interfaces = [intf for intf in task.host["facts"] if task.host["facts"][intf]['counters']['in_crc_errors'] >= 10]
 
-    # Write the data to the CSV file
+    #Write the data to the CSV file
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for intf in interfaces:
             in_crc_errors = task.host['facts'][intf]['counters']['in_crc_errors']
             writer.writerow([hostname, intf, in_crc_errors])
+            table.add_row(hostname, intf, str(in_crc_errors))
+    
 
+    
 my_results = nr.run(task=crc_error)
+
+
+#Display table with CRC error details
+console = Console()
+console.print(table)
+
+rprint("[bright_green] \n Script has finshed [/bright_green]")
