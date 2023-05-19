a4fbda34e8a3aeba1ca3406f3f3196193ddb9759
Bharat Singh Panwar <55885130+bsingh23@users.noreply.github.com>
2023-05-19 13:57:46 +0200

    Update crc_error_v1.0.py
    added CRC error as dynamic value

--- crc_error_v1.0.py
+++ crc_error_v1.0.py
@@ -8,26 +8,29 @@ from rich.console import Console, ConsoleThreadLocals
 from rich.table import Table
 from rich import print as rprint
 
-nr = InitNornir(config_file="/net_ops/nornir/config_prod.yaml")
+nr = InitNornir(config_file="config_prod.yaml")
 
-#Define the header row for the CSV file
+# Define the minimum number of CRC to add in report
+crc_error_count = input("\nPlease enter the minimum number of CRC error: ")
+
+# Define the header row for the CSV file
 header = ["Hostname", "Interface", "CRC Error"]
 
-#Define the parametsr for RICH Table creation
+# Define the parametsr for RICH Table creation
 table = Table(title="Device CRC ERROR Details \n")
 table.add_column("Hostname", justify="right", style="cyan", no_wrap=True)
 table.add_column("Interface", justify="right", style="yellow", no_wrap=True)
 table.add_column("CRC-Error", style="bold red")
 
-rprint(f"[gold3] Data is being collected. Please wait !!! \n [/gold3]")
+rprint(f"[gold3] \n Data is being collected. Please wait !!! \n [/gold3]")
 
-#Get the current date and time as a string to use in the file name
+# Get the current date and time as a string to use in the file name
 time_now = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-#Create the file name using the current date and time
+# Create the file name using the current date and time
 filename = (f"crcerror_{time_now}.csv")
 
-#Check if the file already exists; if not, create the file and write the header row
+# Check if the file already exists; if not, create the file and write the header row
 if not os.path.isfile(filename):
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
@@ -40,10 +43,10 @@ def crc_error(task):
     
     hostname = task.host.name
 
-    #Filter interfaces that have CRC errors (List comprehension)
-    interfaces = [intf for intf in task.host["facts"] if task.host["facts"][intf]['counters']['in_crc_errors'] >= 10]
+    # Filter interfaces that have CRC errors (List comprehension)
+    interfaces = [intf for intf in task.host["facts"] if task.host["facts"][intf]['counters']['in_crc_errors'] >= int(crc_error_count)]
 
-    #Write the data to the CSV file
+    # Write the data to the CSV file
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for intf in interfaces:
